6e7b9bcb4f51544eef4a91dcfc98979e2b8e9310
Tommy840602 <84989346+Tommy840602@users.noreply.github.com>
2024-09-07 11:13:20 +0800

    nginx_demo

---
+++ nginx_test/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import socket
+
+app=FastAPI()
+
+@app.get("/")
+def root():
+    return {"id":socket.gethostname()}
---
+++ test/test.py
@@ -0,0 +1,14 @@
+import requests
+
+MAX_REQUESTS=100
+
+data={}
+
+for i in range(100):
+    res=requests.get("http://127.0.0.1:8000/")
+    id=res.json()["id"]
+    if data.get(id):
+        data[id]=data[id]+1
+    else:
+        data[id]=1
+print(data)
